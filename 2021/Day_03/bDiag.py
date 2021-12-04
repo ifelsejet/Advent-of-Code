ae696d1d8599022c55c4ef6971946150e095f6c2
Jatnael Montes <ifelsejet@gmail.com>
2021-12-03 20:51:59 -0800

    Update bDiag.py

--- 2021/Day_03/bDiag.py
+++ 2021/Day_03/bDiag.py
@@ -1,6 +1,49 @@
-my_file = open("C:\\Users\\ifels\\Documents\\GitHub\\Advent-of-Code\\2021\\Day_03\\test.txt", "r")
-content_list = my_file.readlines()
+from collections import Counter
+#my_file = "C:\\Users\\ifels\\Documents\\GitHub\\Advent-of-Code\\2021\\Day_03\\input.txt"
+#content_list = my_file.readlines().strip().split('\n')
+content_list = [x for x in open('C:\\Users\\ifels\\Documents\\GitHub\\Advent-of-Code\\2021\\Day_03\\input.txt').read().strip().split('\n')]
+
 print(content_list)
+
+"""
+PART 1
+theta = ''
+epsilon = ''
+for i in range(len(content_list[0])):
+	common = Counter([x[i] for x in content_list])
+	if common['0'] > common['1']:
+		theta += '0'
+		epsilon += '1'
+	else:
+		theta += '1'
+		epsilon += '0'
+print(int(theta,2)*int(epsilon,2))
+"""
+
+theta = ''
+epsilon = ''
+for i in range(len(content_list[0])):
+	common = Counter([x[i] for x in content_list])
+	if common['0'] > common['1']:
+	    content_list = [x for x in content_list if x[i] == '0']
+	else:
+		content_list = [x for x in content_list if x[i] == '1']
+	theta = content_list[0]
+print(common)
+content_list = [x for x in open('C:\\Users\\ifels\\Documents\\GitHub\\Advent-of-Code\\2021\\Day_03\\input.txt').read().strip().split('\n')] #reset
+for i in range(len(content_list[0])):
+	common = Counter([x[i] for x in content_list])
+
+	if common['0'] > common['1']:
+		content_list = [x for x in content_list if x[i] == '1']
+	else:
+		content_list = [x for x in content_list if x[i] == '0']
+	if content_list:
+		epsilon = content_list[0]
+print(int(theta,2)*int(epsilon,2)) #int(x, BASE)
+
+
+"""
 i = 0
 nums0= 0
 nums1=0
@@ -29,5 +72,5 @@ for i in content_list:
 print(nums0)
 print(nums1)
 print(convList)
-
+"""
     
